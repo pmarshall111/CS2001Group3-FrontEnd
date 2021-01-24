c5f189304fb74a6432169cb2a5584fabfb3e6cce
zahidali81 <zahid.alia380@gmail.com>
2021-01-24 19:19:37 +0000

    Medication details page is done

--- src/App.js
+++ src/App.js
@@ -9,15 +9,23 @@ import EmailPreview from "./email/EmailPreview";
 import EmailPage from "./email/EmailPage";
 import EmailConfirmation from "./email/EmailConfirmation";
 import EmailInquiry from "./email/EmailInquiry";
-
+import MedicationDetailsPage from "./medication/MedicationDetailsPage";
+import ListDetailsComponent from "./ListDetailsCompoenet";
 function App() {
   return (
+      <div>
+      <ListDetailsComponent/>
+
+        
     <Router>
+        <Route path="/MedicationDetailsPage" component={MedicationDetailsPage}/>
         <Route exact path="/" component={EmailPage} />
         <Route path="/email/confirmation" component={EmailConfirmation} />
         <Route path="/email/inquiry" component={EmailInquiry} />
         <Route path="/pharmacy" component={PharmacyPage} />
     </Router>
+
+    </div>
   );
 }
 
---
+++ src/ListDetailsCompoenet.jsx
@@ -0,0 +1,60 @@
+import React, {Component} from "react";
+import MedicationDetailsService from "./MedicationsDetails";
+class ListDetailsComponent extends Component {
+    constructor() {
+        super();
+        this.state = {
+            medication:[]
+        }
+
+    }
+
+componentDidMount(){
+    MedicationDetailsService.getDetails().then((res)=>
+        {
+            this.setState({medication: res.data});
+        }
+    );
+}
+
+    render() {
+        return(
+            <div>
+                <h2 className = "text-center">Medication Detail</h2>
+
+                <div className = "row"> 
+                    <table className = "table table-strped table-bordered">
+                        <tbody>
+                          <tr>
+                              <th>Description</th>
+                                <th>Dosage</th>
+                                <th>Class</th>
+                                <th>Name</th>
+                                <th>Pharmacy</th>
+                                <th>Count</th>
+                          </tr>
+                        </tbody>
+
+                        <tbody>
+
+                            {
+                                this.state.medication.map(
+                                    medication=>
+                                    <tr key= {medication.id}>
+                                         <td>{medication.dosage}</td>
+                                         <td>{medication.medicationClass}</td>
+                                        <td>{medication.medicationName}</td>
+                                        <td>{medication.pharmacyName}</td>
+                                        <td>{medication.prescriptionCount}</td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+}
+ 
+export default ListDetailsComponent;
