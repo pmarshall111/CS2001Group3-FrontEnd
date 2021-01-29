6564263199328c0a4923b3e4957ed0ca0081982e
zahidali81 <zahid.alia380@gmail.com>
2021-01-29 20:12:38 +0000

    The Home page is all done with React Material UI which can be linked to other pages

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -3,17 +3,20 @@ import {
   Container,
   Grid,
   Button,
-  Typography,
+  // Typography,
   Card,
-  CardActions,
   CardContent,
+  CardActions,
+  CardActionArea,
+  CardMedia,
+  CardHeader,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100vw",
-    height: "100vh",
+    height: "110vh",
     backgroundColor: theme.palette.grey[200],
     paddingTop: theme.spacing(5),
   },
@@ -24,71 +27,100 @@ function HomePage() {
 
   return (
     <Container className={classes.root}>
-      <h1>Dashboard</h1>
+      <h1>Welcome to CareRUs</h1>
       <Grid container spacing={3}>
-        <Grid item sm={3}>
-          <Card>
-            <CardContent>
-              <Typography variant="h5">Residents</Typography>
-              <Typography variant="subtitle1">Lorem </Typography>
-            </CardContent>
+        <Grid item sm={4}>
+          <Card variant="outlined">
+            <CardHeader title="Residents" />
+            <CardActionArea>
+              <CardMedia
+                image="https://rg-group.co.uk/wp-content/uploads/2019/08/CareHome1.jpg"
+                style={{ height: 200 }}
+              />
+              <CardContent></CardContent>
+            </CardActionArea>
             <CardActions>
-              <Button>Read More</Button>
+              <Button>View Residents</Button>
             </CardActions>
           </Card>
         </Grid>
-        <Grid item sm={3}>
+
+        <Grid item sm={4}>
           <Card>
-            <CardContent>
-              <Typography variant="h5">Medications</Typography>
-              <Typography variant="subtitle1">Lorem </Typography>
-            </CardContent>
+            <CardHeader title="Medications" />
+            <CardActionArea>
+              <CardMedia
+                image="https://www.practicalpainmanagement.com/sites/default/files/imagecache/lightbox-large/images/2016/07/18/14253266_M.jpg"
+                style={{ height: 200 }}
+              />
+              <CardContent></CardContent>
+            </CardActionArea>
             <CardActions>
-              <Button>Read More</Button>
+              <Button>View Medications</Button>
             </CardActions>
           </Card>
         </Grid>
-        <Grid item sm={3}>
+
+        <Grid item sm={4}>
           <Card>
-            <CardContent>
-              <Typography variant="h5">Emails</Typography>
-              <Typography variant="subtitle1">Lorem </Typography>
-            </CardContent>
+            <CardHeader title="Emails" />
+            <CardActionArea>
+              <CardMedia
+                image="https://cdn.mos.cms.futurecdn.net/MCTaifsDFHvU7pJ9uKmjzY.jpg"
+                style={{ height: 200 }}
+              />
+              <CardContent></CardContent>
+            </CardActionArea>
             <CardActions>
-              <Button>Read More</Button>
+              <Button>View Emails</Button>
             </CardActions>
           </Card>
         </Grid>
-        <Grid item sm={3}>
+
+        <Grid item sm={4}>
           <Card>
-            <CardContent>
-              <Typography variant="h5">Care workers</Typography>
-              <Typography variant="subtitle1">Lorem </Typography>
-            </CardContent>
+            <CardHeader title="Care Workers" />
+            <CardActionArea>
+              <CardMedia
+                image="https://helpr.org.uk/wp-content/uploads/2018/04/caring-staff.jpg"
+                style={{ height: 200 }}
+              />
+              <CardContent></CardContent>
+            </CardActionArea>
             <CardActions>
-              <Button>Read More</Button>
+              <Button>View Care Workers</Button>
             </CardActions>
           </Card>
         </Grid>
-        <Grid item sm={3}>
+
+        <Grid item sm={4}>
           <Card>
-            <CardContent>
-              <Typography variant="h5">Daily Tasks</Typography>
-              <Typography variant="subtitle1">Lorem </Typography>
-            </CardContent>
+            <CardHeader title="Daily Tasks" />
+            <CardActionArea>
+              <CardMedia
+                image="https://thumbs.dreamstime.com/b/task-list-icon-logo-illustration-summer-icons-set-outline-holiday-tour-travel-vector-simple-modern-graphic-flat-design-159161578.jpg"
+                style={{ height: 200 }}
+              />
+              <CardContent></CardContent>
+            </CardActionArea>
             <CardActions>
-              <Button>Read More</Button>
+              <Button>View Tasks</Button>
             </CardActions>
           </Card>
         </Grid>
-        <Grid item sm={3}>
+
+        <Grid item sm={4}>
           <Card>
-            <CardContent>
-              <Typography variant="h5">Pharmacies</Typography>
-              <Typography variant="subtitle1">Lorem </Typography>
-            </CardContent>
+            <CardHeader title="Pharmacies" />
+            <CardActionArea>
+              <CardMedia
+                image="https://mmo.aiircdn.com/326/5e8b2443bb37b.jpg"
+                style={{ height: 200 }}
+              />
+              <CardContent></CardContent>
+            </CardActionArea>
             <CardActions>
-              <Button>Read More</Button>
+              <Button>View Pharmacies</Button>
             </CardActions>
           </Card>
         </Grid>
